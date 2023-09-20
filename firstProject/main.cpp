663dbb25131c7fcc138155d01affbd849d99b863
azizthiampro@gmail.com <azizthiampro@gmail.com>
2023-09-20 19:53:38 +0200

    Initial Commit

---
+++ firstProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  firstProject
+//
+//  Created by MACBOOK M1 2021 on 20/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
